3a2901bb0ee9d8c5519dcdf51e2e39d4e7095ba5
Tigist D <tigist2274@gmail.com>
2025-01-11 11:45:49 -0600

    lesson#2 homework:004

--- lesson_2/practice_004_variables_naming.py
+++ lesson_2/practice_004_variables_naming.py
@@ -1,12 +1,14 @@
 # Look at the precode and correct all invalid identifiers
 
-1st_number = 34
-Last-number= 42.67
+first_number = 34
+Last_number= 42.67
 age = 25
-theanswer@ = 42
-Class = 1.8
-tax%rate = 0.07          
-fav colorcode = 4455
+the_answer= 42
+Class_ = 1.8
+tax_rate = 0.07
+fav_colorcode = 4455
 ShoeSize = 4
+
+
 
 # Send your code to the chat
