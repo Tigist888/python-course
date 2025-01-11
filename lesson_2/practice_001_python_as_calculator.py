0d09986493ce0a22bbc167c70b04365dc15f25f1
Tigist D <tigist2274@gmail.com>
2025-01-10 23:50:24 -0600

    lesson #2 homework :001

--- lesson_2/practice_001_python_as_calculator.py
+++ lesson_2/practice_001_python_as_calculator.py
@@ -1,16 +1,57 @@
 # Try the following expressions in Terminal and share the results in the chat.
+#1 + 1
+x=1
+y=1
+print(x+y)
 
-# 1 + 1
 # 50 - 90
-# 28 * 3
-# -1 * 5
-# 478 / 2
-# 2 ** 8
-# 332 + 75.6
-# 500 // 2 ** 3
-# -5 + 2
-# 100 % 3
-# 100 // 3
-# 1 / 3
+a=50
+b=90
+print(a-b)
 
+# 28 * 3
+l=28
+m=3
+print(l * m)
+
+# -1 * 5
+c=-1
+d=5
+print(c*d)
+
+# 478 / 2
+f=478
+h=2
+print(f/h)
+
+# 2 ** 8
+g=2
+m=8
+print(g**m)
+# 332 + 75.6
+n=332
+p=75.6
+print(n + p)
+# 500 // 2 ** 3
+u=500
+q=2
+r=3
+print(u//2**3)
+# -5 + 2
+c_1=-5
+c_2=2
+print(c_1+c_2)
+# 100 % 3
+d_1=100
+d_2=3
+print(d_1 % d_2)
+# 100 // 3
+dd=100
+ee=3
+print(dd//ee)
+
+# 1 / 3
+m_1=1
+m_2=2
+print(m_1 / m_2)g
 # If more time is available, try different values and see what happens
