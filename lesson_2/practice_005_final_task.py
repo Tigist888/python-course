fa7b59129b5a39fc934ef4c18e2e511f1b23dcdc
Tigist D <tigist2274@gmail.com>
2025-01-11 11:51:34 -0600

    lesson#2 homework:005

--- lesson_2/practice_005_final_task.py
+++ lesson_2/practice_005_final_task.py
@@ -8,6 +8,10 @@ entertainment = 200
 
 # Calculate the total monthly expenses and determine the
 # percentage of rent relative to the total expenses.
+total_monthly_expanse=rent+utilities+groceries+entertainment
+print(total_monthly_expanse)
+rent_perentag=(rent/total_monthly_expanse)*100
+print(rent_perentag)
 
 # Hint: to count the percentage of rent, count total monthly
 # expenses first, then divide expenses for rent by the total
