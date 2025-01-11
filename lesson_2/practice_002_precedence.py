9fe78bdce95fac295bb3647758fca7570482994b
Tigist D <tigist2274@gmail.com>
2025-01-11 11:37:34 -0600

    lessen#2 homework 002

--- lesson_2/practice_002_precedence.py
+++ lesson_2/practice_002_precedence.py
@@ -2,9 +2,40 @@
 # why the difference in results
 
 # >>> 4 + 100 / 10 * 5
+a=4
+b=100
+c=10
+d=5
+result_1=(a + b /c*d)
+print(result_1)
 
 # >>> 4 + (100 / (10 * 5))
+a=4
+b=100
+c=10
+d=5
+
+total=( a +(b /(c * d  )))
+print(total)
+# precedence
 
 # >>> (4 + 100 / 10) * 5
+a = 4
+b = 100
+c = 10
+d = 5
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+result=(a + b/c) * d
+print(result)
 
 # >>> ((4 + 100) / 10) * 5
+
+a = 4
+b = 100
+c = 10
+d = 5
+total_2=((a +b) / c) * d
+print(total_2)
