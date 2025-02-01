a49fab3ebdd59010e22797b7aba9cf09a4f8c35b
Tigist D <tigist2274@gmail.com>
2025-02-01 15:42:09 -0600

    lesson#7 homework update

--- lesson_6/sandbox.py
+++ lesson_6/sandbox.py
@@ -1,13 +1,3 @@
-# Get input from the user
-user_input = input('Give me a phrase')
-
-# Split user input into words
-words = user_input.split()
-print(words)
-
-# Reverse the list and print it
-reversed_words = " ".join(reversed(words))
-print(reversed_words)
-
-# Print the length of the words list
-print(len(words))
+color = ['blue', 'red', 'white']
+index = 0  # Define an index (e.g., 1 to access 'red')
+print(color[index])
--- lesson_7/practice_004_while.py
+++ lesson_7/practice_004_while.py
@@ -2,6 +2,8 @@
 # it prints out the number of characters in the name.
 # Modify the code so it will exit if the user enters an empty string.
 
-while ???:
+while True:
    name = input("Name: ")
+   if name == " ":
+      break
    print(f'{name} has {len(name)} characters.')
